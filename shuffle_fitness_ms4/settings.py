6168329e2804abf4766b2d6cb592f15b42fd1469
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-09-23 14:15:34 +0000

    Added header and content to index.html

--- shuffle_fitness_ms4/settings.py
+++ shuffle_fitness_ms4/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87&gd!cg!lyc-bxqq!+#0v$lc1wc6&d-ki3k0_jef&s8$%y^8d'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
