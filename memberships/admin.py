a5350837f96295a3f9ba770badf8edddff290d59
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-09-29 19:39:33 +0000

    Customised admin

--- memberships/admin.py
+++ memberships/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Memberships, Category
 
 
-admin.site.register(Memberships)
-admin.site.register(Category)
+class MembershipsAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'price_yearly',
+        'price_monthly',
+        'price_quarterly',
+        'rating',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'display_name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Memberships, MembershipsAdmin)
