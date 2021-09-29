7721767f1a5e81199b26efdaf2cc71f800252710
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-09-29 22:45:43 +0000

    Added search functionality for names and descriptions

--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Membership
 
 
@@ -6,9 +8,21 @@ def all_memberships(request):
     """ A view to show all the memberships, including search queries"""
 
     memberships = Membership.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not enter any search criteria!")
+                return redirect(reverse('memberships'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            memberships = memberships.filter(queries)
 
     context = {
         'memberships': memberships,
+        'search_term': query,
     }
 
     return render(request, 'memberships/memberships.html', context)
