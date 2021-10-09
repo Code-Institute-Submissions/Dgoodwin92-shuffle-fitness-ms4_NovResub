0ef3a6115d6d51ce9f8b4804e8c508468b7e4b94
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-10-09 22:26:28 +0000

    Webhook handlers completed

--- checkout/models.py
+++ checkout/models.py
@@ -8,7 +8,6 @@ from memberships.models import Membership
 
 
 class Order(models.Model):
-
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -22,6 +21,8 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """ Generate a random, unique order number using UUID """
