e11eb39ca6f6f2eb085c0f4315e549a125d3976b
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-11-14 20:56:34 +0000

    Created membership form for admin

---
+++ memberships/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Membership, Category
+
+class MembershipForm(forms.ModelForm):
+
+    class Meta:
+        model = Membership
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        display_name = [(c.id, c.get_display_name()) for c in categories]
+
+        self.fields['category'].choices = display_name
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
