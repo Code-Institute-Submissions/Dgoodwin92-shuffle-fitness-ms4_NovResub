8eaacfbe9c27f022654ab6ab77136a9a17f2ddaa
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-11-19 02:33:13 +0000

    Corrected error within custom_storages.py

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3BotoStorage
 
 
 class StaticStorage(S3BotoStorage):
-    location settings.STATICFILES_LOCATION
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3BotoStorage):
-    location settings.MEDIAFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
